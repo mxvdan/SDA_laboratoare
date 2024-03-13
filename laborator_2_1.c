0822d3b0c23f3ce779d37d52e74a0990afe4d851
mxvdan <maximovdan@icloud.com>
2024-03-13 21:19:16 +0200

    changes

--- laborator_2_1.c
+++ laborator_2_1.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Car { 
+typedef struct car { 
     int ID;
     char marca[50];
     char model[50];
     int an_producere;
     char stare[20];
-    struct Car *next;
-};
+    struct car *next;
+}Car;
 
-struct Car *readCar(){
-    struct Car *car = (struct Car*)malloc(sizeof(struct Car));
+ Car *readCar(){
+    Car *car = (Car*)malloc(sizeof(Car));
     if (car == NULL){
         printf("Eroare alocare");
         exit(1);
@@ -20,29 +20,29 @@ struct Car *readCar(){
     printf("\nDati ID-ul masinii: ");
     scanf("%d", &car->ID);
     printf("\nDati marca masinii: ");
-    scanf("%s", &car->marca);
+    scanf("%s", car->marca);
     printf("\nDati modelul masinii: ");
-    scanf("%s", &car->model);
+    scanf("%s", car->model);
     printf("\nDati anul de producere: ");
     scanf("%d", &car->an_producere);
     printf("\nDati starea masinii: ");
-    scanf("%s", &car->stare);
+    scanf("%s", car->stare);
     car->next = NULL;
     return car;
 }
 
-void printList(struct car *head){
-    struct Car *current = head;
+void printList(Car *head){
+    Car *current = head;
     printf("Lista de masini:\n");
     while(current != NULL){
-        printf("ID: %d, Marca: %s, Modelul: %s, Anul: %d, Starea: %s",current->ID, current->marca, current->model, current->an_producere, current->stare);
+        printf("ID: %d, Marca: %s, Modelul: %s, Anul: %d, Starea: %s\n",current->ID, current->marca, current->model, current->an_producere, current->stare);
         current = current->next;
     }
 }
 
-void freeList(struct car **head){
-    struct Car *current =  *head;
-    struct Car *next;
+void freeList(Car **head){
+    Car *current = *head;
+    Car *next;
     while(current != NULL){
         next = current->next;
         free(current);
